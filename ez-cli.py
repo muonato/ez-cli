b47568dd015c9f31643f04a928f3f4b47482cc9b
Tommi Muona <43796235+muonato@users.noreply.github.com>
2018-10-04 00:23:13 +0300

    Add files via upload

---
+++ ez-cli.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import subprocess, readline
+
+CLI_CMD = ["HELP", "QUIT"]
+
+class CLIMate():
+	def __init__(self):
+		self.run = None
+		self.arg = ""
+		self.fnc = [("HELP", self.help), ("QUIT", self.quit)]
+
+		readline.parse_and_bind('set editing-mode vi')
+		readline.parse_and_bind('tab: complete')
+		readline.set_completer(self.completer)
+
+	def completer(self, text, state):
+		options = [i for i in CLI_CMD if i.startswith(text.upper())]
+		if (state < len(options)):
+			return options[state]
+		else:
+			return None
+ 
+	def get_args(self):
+		self.run = self.sub_call
+		self.arg = input("\nez-cli> ")
+		if (self.arg == ""):
+			self.get_help("nothing")
+		else:
+			for f in self.fnc:
+				if (self.arg.startswith(f[0])):
+					self.run = f[1]
+		self.run(self.arg)
+
+	def quit(self, parm):
+		print("Goodbye.", "")
+
+	def help(self, parm):
+		print("EZ-CLI COMMANDS\n\tHELP - this\n\tQUIT - exit")
+
+	def sub_call(self, parm):
+		try:
+			subprocess.call(parm, shell=True)
+		except KeyboardInterrupt:
+			print("Terminated by keystroke")
+
+def main():
+	cli = CLIMate()
+	while not (cli.run == cli.quit):
+		try:
+			cli.get_args()
+		except KeyboardInterrupt:
+			print("Type QUIT to exit EZ-CLI")
+
+if __name__ == "__main__":
+	main()
